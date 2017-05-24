ed190332c1ea73d5a38c28dd5284d475b97e02e0
bzreinhardt <bzreinhardt@gmail.com>
2017-05-24 02:12:32 -0700

    working google transcriber

--- keystone_asr.py
+++ keystone_asr.py
@@ -2,6 +2,7 @@
 
 from pprint import pprint
 import imp
+import argparse
 
 import os, sys, inspect
 import speech_recognition as sr
@@ -9,18 +10,31 @@ import speech_recognition as sr
 # obtain path to "english.wav" in the same folder as this script
 from os import path
 import json
+import time
 import pdb
 from credentials import credentials
 
-FILE_NAME = "Kon"
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "%s.wav" % FILE_NAME)
-TRANSCRIPT_FILE = path.join(path.dirname(path.realpath(__file__)), "%s.json" % FILE_NAME)
-DURATION = 120
+FILE_NAME = "ES2016a.Mix-Headset"
+DIR_NAME = "/Users/Zaaron/Data/audio"
+AUDIO_FILE = path.join(DIR_NAME, "%s.wav" % FILE_NAME)
+CLOUD_AUDIO_FILE = "gs://illiad-audio/ES2016a.Mix-Headset.flac"
+TRANSCRIPT_FILE = path.join(DIR_NAME, "%s_google_transcript.json" % FILE_NAME)
+DURATION = 59
 # AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "french.aiff")
 # AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "chinese.flac")
 
 
-
+def wav_to_flac(audio_file, duration=None):
+    (directory, filename) = os.path.split(audio_file)
+    name = ".".join(filename.split(".")[:-1])
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio = r.record(source, duration=duration)
+    out_name = "%s/%s.flac" % (directory, name)
+    if duration:
+        out_name = "%s/%s_%ds.flac" % (directory, name, duration)
+    r.write_flac(audio, out_name)
+    return out_name
 
 def assign_speaker_labels(words, speakers):
     # assumes words and speakers are all in temporal order
@@ -46,7 +60,7 @@ def words_from_json(watson_json, name=""):
             start = word[1]
             end = word[2]
             confidence = result['alternatives'][0]['word_confidence'][i][1]
-            word = {'text': text, 'starttime': start, 'endtime': end, 'confidence': confidence}
+            word = {'id': word_id, 'text': text, 'starttime': start, 'endtime': end, 'confidence': confidence}
             words.append(word)
 
         #assign speakers
@@ -90,9 +104,86 @@ def run_watson(audio_file, duration=None, transcript_file=''):
     return transcript
 
 
+def run_google(audio_file, duration=None, transcript_file=''):
+    # recognize speech using Google Speech Recognition
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio = r.record(source, duration=duration)
+    """
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY", show_all=True)`
+        # instead of `r.recognize_google(audio, show_all=True)`
+        print("Google Speech Recognition results:")
+        pprint(r.recognize_google(audio, show_all=True))  # pretty-print the recognition result
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    """
+    # recognize speech using Google Cloud Speech
+    with open(credentials["google"]["file"], 'r') as f:
+        GOOGLE_CLOUD_SPEECH_CREDENTIALS = f.read()
+    try:
+        print("Google Cloud Speech recognition results:")
+        result = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS,
+                                        show_all=True)  # pretty-print the recognition result
+        pprint(result)
+        if len(transcript_file) > 0:
+            with open(transcript_file, 'w') as f:
+                f.write(json.dumps(result))
+
+    except sr.UnknownValueError:
+        print("Google Cloud Speech could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Cloud Speech service; {0}".format(e))
+
+def transcribe_gcs(gcs_uri, name=""):
+    """Asynchronously transcribes the audio file specified by the gcs_uri."""
+    from google.cloud import speech
+    speech_client = speech.Client()
+
+    audio_sample = speech_client.sample(
+        content=None,
+        source_uri=gcs_uri,
+        encoding='FLAC')
+        #sample_rate_hertz=16000)
+
+    operation = audio_sample.long_running_recognize('en-US')
+
+    retry_count = 100
+    while not operation.complete:
+        retry_count -= 1
+        time.sleep(2)
+        operation.poll()
+
+    if not operation.complete:
+        print('Operation not complete and retry limit reached.')
+        return
+
+    alternatives = operation.results
+    words = []
+    for i, alternative in enumerate(alternatives):
+        words.append({"id":"%s_%d"%(name, i), "text":alternative.transcript, "confidence":alternative.confidence})
+    return words
+
 if __name__ == "__main__":
-    with open(TRANSCRIPT_FILE, 'r') as f:
-        words = words_from_json(json.loads(f.read()))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", default="test")
+    parser.add_argument("--transcribe", action="store_true")
+    parser.add_argument("--convert", action="store_true")
+    parser.add_argument("--audio_file")
+
+    args = parser.parse_args()
+    if args.transcribe:
+        transcript_file = "%s_google_transcript.json" % args.name
+        audio_file = args.audio_file
+        name = args.name
+        transcript = transcribe_gcs(audio_file, name=name)
+        with open(transcript_file, 'w') as f:
+            f.write(json.dumps(transcript))
+    if args.convert:
+        wav_to_flac(args.audio_file)
 
 
 
@@ -104,29 +195,8 @@ except sr.UnknownValueError:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
     print("Sphinx error; {0}".format(e))
-
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY", show_all=True)`
-    # instead of `r.recognize_google(audio, show_all=True)`
-    print("Google Speech Recognition results:")
-    pprint(r.recognize_google(audio, show_all=True))  # pretty-print the recognition result
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-# recognize speech using Google Cloud Speech
-GOOGLE_CLOUD_SPEECH_CREDENTIALS = "INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"
-try:
-    print("Google Cloud Speech recognition results:")
-    pprint(r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS, show_all=True))  # pretty-print the recognition result
-except sr.UnknownValueError:
-    print("Google Cloud Speech could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Cloud Speech service; {0}".format(e))
-
+    
+    
 # recognize speech using Wit.ai
 WIT_AI_KEY = "INSERT WIT.AI API KEY HERE"  # Wit.ai keys are 32-character uppercase alphanumeric strings
 try:
