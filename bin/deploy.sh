cd8c5ad73dc2cbc2b9d3417eaec98852a0a32376
Noah Tye <hi@noahtye.com>
2017-06-16 11:24:16 -0700

    iterating on deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ssh -i ~/.ssh/adam.pem ubuntu@54.183.66.153 'bash ~/keystone/bin/run_in_prod.sh'
--- bin/run_in_prod.sh
+++ bin/run_in_prod.sh
@@ -10,6 +10,13 @@ git pull origin master
 sed -i 's/DEBUG = True/DEBUG = False/g' webapp/webapp/settings.py
 
 cd /home/ubuntu/keystone/webapp
+export PATH=/home/ubuntu/miniconda3/bin:$PATH
+set +x
+echo '= source ~/keystone_env.sh'
+source ~/keystone_env.sh
+echo '= source activate keystone'
 source activate keystone
-killall uwsgi
+echo '= killing any uwsgi processes'
+killall -q uwsgi
+set -x
 uwsgi --socket /tmp/evoke.sock --module webapp.wsgi --chmod-socket=666 &> ~/logs/uwsgi.log &
