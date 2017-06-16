85208a1a694b0a90675f6ce7f9678ee7cadbb621
Noah Tye <hi@noahtye.com>
2017-06-16 11:36:25 -0700

    get rid of set -e

--- bin/run_in_prod.sh
+++ bin/run_in_prod.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
-set -x
-
+echo '= pulling latest code'
 cd /home/ubuntu/keystone
 git reset --hard HEAD
 git checkout master
 git pull origin master
 
+echo '= setting DEBUG = True'
 sed -i 's/DEBUG = True/DEBUG = False/g' webapp/webapp/settings.py
 
 cd /home/ubuntu/keystone/webapp
 export PATH=/home/ubuntu/miniconda3/bin:$PATH
-set +x
 echo '= source ~/keystone_env.sh'
 source ~/keystone_env.sh
 echo '= source activate keystone'
 source activate keystone
 echo '= killing any uwsgi processes'
 killall uwsgi || true
-set -x
+echo '= running uwsgi for webapp'
 uwsgi --socket /tmp/evoke.sock --module webapp.wsgi --chmod-socket=666 &> ~/logs/uwsgi.log &
