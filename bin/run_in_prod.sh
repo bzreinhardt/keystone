d907f56addca47e51863526a31d2d804a466969d
Noah Tye <hi@noahtye.com>
2017-06-16 11:10:07 -0700

    run in prod script: set DEBUG flag

--- bin/run_in_prod.sh
+++ bin/run_in_prod.sh
@@ -3,8 +3,13 @@
 set -x
 
 cd /home/ubuntu/keystone
+git reset --hard HEAD
 git checkout master
 git pull origin master
 
+sed -i 's/DEBUG = True/DEBUG = False/g' webapp/webapp/settings.py
+
+cd /home/ubuntu/keystone/webapp
 source activate keystone
+killall uwsgi
 uwsgi --socket /tmp/evoke.sock --module webapp.wsgi --chmod-socket=666 &> ~/logs/uwsgi.log &
